bdd31b7c0546a8fb33501f1b3c40a7dbfeaf0d85
Alban Jubert <ajubert@simplonprod.co>
2022-02-26 12:10:48 +0100

    Fix for issue #101

--- src/services/OembedService.php
+++ src/services/OembedService.php
@@ -11,9 +11,9 @@
 namespace wrav\oembed\services;
 
 use craft;
+use craft\base\Component;
 use craft\helpers\Template;
 use craft\helpers\UrlHelper;
-use craft\base\Component;
 use DOMDocument;
 use Embed\Adapters\Adapter;
 use Embed\Embed;
@@ -36,6 +36,23 @@ class OembedService extends Component
     /**
      * @param $url
      * @param array $options
+     * @return string
+     */
+    public function render($url, array $options = [])
+    {
+        /** @var Media $media */
+        $media = $this->embed($url, $options);
+
+        if (!empty($media)) {
+            return Template::raw(isset($media->code) ? $media->code : '');
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param $url
+     * @param array $options
      * @return Media|string
      */
     public function embed($url, array $options = [])
@@ -45,7 +62,7 @@ class OembedService extends Component
         } catch (\Exception $exception) {
             $hash = '';
         }
-        $cacheKey = $url.'_'.$hash;
+        $cacheKey = $url . '_' . $hash;
 
         if (Oembed::getInstance()->getSettings()->enableCache && Craft::$app->cache->exists($cacheKey)) {
             return \Craft::$app->cache->get($cacheKey);
@@ -82,7 +99,11 @@ class OembedService extends Component
             // Wrapping to be safe :)
             try {
                 $dom = new DOMDocument;
-                $dom->loadHTML($media->getCode());
+                $code = $media->getCode();
+                if (empty($code)) {
+                    $code = Utils::iframe($media->url);
+                }
+                $dom->loadHTML($code);
 
                 $iframe = $dom->getElementsByTagName('iframe')->item(0);
                 $src = $iframe->getAttribute('src');
@@ -94,15 +115,15 @@ class OembedService extends Component
                     $src .= "?";
                 }
 
-                if(!empty($options['params'])) {
-                    foreach((array)$options['params'] as $key => $value) {
-                        $src = preg_replace('/\?(.*)$/i', '?'.$key.'='. $value .'&${1}', $src);
+                if (!empty($options['params'])) {
+                    foreach ((array)$options['params'] as $key => $value) {
+                        $src = preg_replace('/\?(.*)$/i', '?' . $key . '=' . $value . '&${1}', $src);
                     }
                 }
 
                 // Autoplay
                 if (!empty($options['autoplay']) && strpos($html, 'autoplay=') === false && $src) {
-                    $src = preg_replace('/\?(.*)$/i', '?autoplay='. (!!$options['autoplay'] ? '1' : '0') .'&${1}', $src);
+                    $src = preg_replace('/\?(.*)$/i', '?autoplay=' . (!!$options['autoplay'] ? '1' : '0') . '&${1}', $src);
                 }
 
                 // Width - Override
@@ -117,21 +138,21 @@ class OembedService extends Component
 
                 // Looping
                 if (!empty($options['loop']) && strpos($html, 'loop=') === false && $src) {
-                    $src = preg_replace('/\?(.*)$/i', '?loop='. (!!$options['loop'] ? '1' : '0') .'&${1}', $src);
+                    $src = preg_replace('/\?(.*)$/i', '?loop=' . (!!$options['loop'] ? '1' : '0') . '&${1}', $src);
                 }
 
                 // Autopause
                 if (!empty($options['autopause']) && strpos($html, 'autopause=') === false && $src) {
-                    $src = preg_replace('/\?(.*)$/i', '?autopause='. (!!$options['autopause'] ? '1' : '0') .'&${1}', $src);
+                    $src = preg_replace('/\?(.*)$/i', '?autopause=' . (!!$options['autopause'] ? '1' : '0') . '&${1}', $src);
                 }
 
                 // Rel
                 if (!empty($options['rel']) && strpos($html, 'rel=') === false && $src) {
-                    $src = preg_replace('/\?(.*)$/i', '?rel='. (!!$options['rel'] ? '1' : '0') .'&${1}', $src);
+                    $src = preg_replace('/\?(.*)$/i', '?rel=' . (!!$options['rel'] ? '1' : '0') . '&${1}', $src);
                 }
 
-                if(!empty($options['attributes'])) {
-                    foreach((array)$options['attributes'] as $key => $value) {
+                if (!empty($options['attributes'])) {
+                    foreach ((array)$options['attributes'] as $key => $value) {
                         $iframe->setAttribute($key, $value);
                     }
                 }
@@ -141,8 +162,7 @@ class OembedService extends Component
 
             } catch (\Exception $exception) {
                 Craft::info($exception->getMessage(), 'oembed');
-            }
-            finally {
+            } finally {
                 if (Oembed::getInstance()->getSettings()->enableCache) {
                     // Cache failed requests only for 15 minutes
                     $duration = $media instanceof FallbackAdapter ? 15 * 60 : 60 * 60;
@@ -161,19 +181,19 @@ class OembedService extends Component
             $youtubePattern = '/(?:http|https)*?:*\/\/(?:www\.|)(?:youtube\.com|m\.youtube\.com|youtu\.be|youtube-nocookie\.com)/i';
             preg_match($youtubePattern, $url, $matches, PREG_OFFSET_CAPTURE);
 
-            if(count($matches)) {
+            if (count($matches)) {
                 $url = preg_replace($youtubePattern, 'https://www.youtube-nocookie.com', $url);
                 $skip = true;
             }
 
-            if(!$skip) {
-                if (strpos($url, 'vimeo.com') !== false ) {
+            if (!$skip) {
+                if (strpos($url, 'vimeo.com') !== false) {
                     if (strpos($url, 'dnt=') === false) {
                         preg_match('/\?.*$/', $url, $matches, PREG_OFFSET_CAPTURE);
-                        if(count($matches)) {
+                        if (count($matches)) {
                             $url = preg_replace('/(\?(.*))$/i', '?dnt=1&${2}', $url);
                         } else {
-                            $url = $url.'?dnt=1';
+                            $url = $url . '?dnt=1';
                         }
                     }
 
@@ -184,22 +204,5 @@ class OembedService extends Component
         }
 
         return $url;
-    }
-
-    /**
-     * @param $url
-     * @param array $options
-     * @return string
-     */
-    public function render($url, array $options = [])
-    {
-        /** @var Media $media */
-        $media = $this->embed($url, $options);
-
-        if (!empty($media)) {
-            return Template::raw(isset($media->code) ? $media->code : '');
-        } else {
-            return null;
-        }
     }
 }
